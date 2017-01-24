3cd4b36fe3c90991e622139e854a1f3eb228e3a7
piramidex <piramidex@gmail.com>
2017-01-23 22:31:05 -0400

    Update editing product accordingly to product insertion

--- Classes/Hooks/class.tx_mscolors_updateproductposthook.php
+++ Classes/Hooks/class.tx_mscolors_updateproductposthook.php
@@ -11,21 +11,24 @@ class tx_mscolors_updateproductposthook {
     error_log('product_id: '.$product_id);
     if (!$product_id) { return; }
 
-    $attributes_ids = $ref->post['tx_multishop_pi1']['attributes_ids'];
+    $attributes_ids = $ref->post['tx_multishop_pi1']['pa_id'];
     $colors = $ref->post['tx_multishop_pi1']['colors'];
+    $is_colors = $ref->post['tx_multishop_pi1']['is_colors'];
     error_log('attributes_ids: '.print_r($attributes_ids, true));
     error_log('colors: '.print_r($colors, true));
     for ($i = 0; $i < count($attributes_ids); $i++) {
-      $update_array = array(
-        'name' => '',
-        'code' => '',
-        'image' => $colors[$i]
-      );
-      $res = $GLOBALS['TYPO3_DB']->exec_UPDATEquery(
-        'tx_mscolors_domain_model_colors',
-        'product_id = '.$product_id.' and attribute_id = '.$attributes_ids[$i], // TODO make this secure with fullQuoteStr
-        $update_array
-      );
+      if ($is_colors[$i] == "1") {
+        $update_array = array(
+          'name' => '',
+          'code' => '',
+          'image' => $colors[$i]
+        );
+        $res = $GLOBALS['TYPO3_DB']->exec_UPDATEquery(
+          'tx_mscolors_domain_model_colors',
+          'product_id = '.$product_id.' and attribute_id = '.$attributes_ids[$i], // TODO make this secure with fullQuoteStr
+          $update_array
+        );
+      }
     }
 
     error_log("updateProductPostHook() - end");
